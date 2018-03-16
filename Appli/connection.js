eec43a1519dcc0c5cdb84135d465ba5a56c28c44
carel <elimbicarel@gmail.com>
2018-03-16 12:00:45 +0100

    connection

--- Appli/connection.js
+++ Appli/connection.js
@@ -10,12 +10,13 @@ MongoClient.connect(url, function(err, client){
   assert.equal(null,err);
   //console.log("connected succesfully to server", client);
   const db= client.db('enron');
-  findDocuments("_sent", db, function(){
+  findDocuments1("rosalee.fleming@enron.com", db, function(){
     client.close();
   });
 });
 
 
+// renvoie les mails d'un folder specifique
 const findDocuments = function(folder, db, callback){
   const collection = db.collection('email');
     collection.find({"folder":folder}).toArray(function(err,docs){
@@ -23,34 +24,44 @@ const findDocuments = function(folder, db, callback){
     console.log("Found docs");
     //console.log(docs);
     docs.forEach(function(doc){
-      console.log(doc.folder);
+      console.log(doc);
     });
     callback(docs);
   });
 }
 
-const findDocuments = function(sender, db, callback){
+
+// renvoie les mails d'un sender specifique
+const findDocuments1 = function(sender, db, callback){
   const collection = db.collection('email');
     collection.find({"sender":sender}).toArray(function(err,docs){
     assert.equal(err,null);
     console.log("Found docs");
     //console.log(docs);
+
     docs.forEach(function(doc){
-      console.log(doc.folder);
+      console.log(doc.text);
+    });
+      console.log(docs.length);
+    callback(docs);
+
+  });
+
+}
+
+
+//renvoie les mails d'un recipient specifique
+const findDocuments2 = function(recipients, db, callback){
+  const collection = db.collection('email');
+    collection.find({"recipients":recipients}).toArray(function(err,docs){
+    assert.equal(err,null);
+    //console.log(docs);
+    docs.forEach(function(doc){
+      console.log(doc);
     });
     callback(docs);
   });
 }
 
-const findDocuments = function(reciepients, db, callback){
-  const collection = db.collection('email');
-    collection.find({"reciepients":reciepients}).toArray(function(err,docs){
-    assert.equal(err,null);
-    console.log("Found docs");
-    //console.log(docs);
-    docs.forEach(function(doc){
-      console.log(doc.folder);
-    });
-    callback(docs);
-  });
-}
+
+//renvoie le nombr de mails dans un folder
